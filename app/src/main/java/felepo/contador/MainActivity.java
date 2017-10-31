c45b9d40d5136ec69c1ac527741d89d4e0702c95
Fernando Poncio <fecrackle@gmail.com>
2017-10-31 00:10:29 -0600

    Dando funcionalidad al EditText y al CheckBox.

--- app/src/main/java/felepo/contador/MainActivity.java
+++ app/src/main/java/felepo/contador/MainActivity.java
@@ -3,6 +3,8 @@ package felepo.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
@@ -10,10 +12,13 @@ public class MainActivity extends Activity {
     //Variable para llevar el numero de veces que se preciona un boton
     public int contador;
 
+    TextView textoResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textoResultado = (TextView) findViewById(R.id.contadorPulsaciones);
 
         contador = 0;
     }
@@ -22,27 +27,57 @@ public class MainActivity extends Activity {
     {
         contador++;
 
-        mostrarContador();
+        if( contador < 0 )
+        {
+            CheckBox casillaNegativo = (CheckBox) findViewById(R.id.negativos);
+
+            if( !casillaNegativo.isChecked() )
+            {
+                contador = 0;
+            }
+        }
+
+        textoResultado.setText("" + contador);
     }
 
     public void decrementarContador(View vista)
     {
         contador--;
 
-        mostrarContador();
+        if( contador < 0 )
+        {
+            CheckBox casillaNegativo = (CheckBox) findViewById(R.id.negativos);
+
+            if( !casillaNegativo.isChecked() )
+            {
+                contador = 0;
+            }
+        }
+
+        textoResultado.setText("" + contador);
     }
 
     public void reiniciarContador(View vista)
     {
-        contador = 0;
+        EditText numeroReset = (EditText) findViewById(R.id.inNumber);
+        CheckBox casillaNegativo = (CheckBox) findViewById(R.id.negativos);
 
-        mostrarContador();
-    }
+        try
+        {
+            contador = Integer.parseInt(numeroReset.getText().toString());
+        }
+        catch(Exception e)
+        {
+            contador = 0;
+        }
 
-    public void mostrarContador()
-    {
-        //Con esto mando a traer el TextView contadorPulsaciones desde la clase R de recursos
-        TextView textoResultado = (TextView) findViewById(R.id.contadorPulsaciones);
+        //Si la casilla no esta seleccionada y el numero es negativo, entonces contador es cero.
+        if( ( !casillaNegativo.isChecked() ) && ( contador < 0 ) )
+        {
+            contador = 0;
+        }
+
+        numeroReset.setText("");
         textoResultado.setText("" + contador);
     }
 }
