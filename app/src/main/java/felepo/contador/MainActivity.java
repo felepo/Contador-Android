40d15619283ae109d0c95d340dc4b6d8fc8db072
Fernando Poncio <fecrackle@gmail.com>
2017-11-01 21:47:44 -0600

    Agregando Escuchadores para el teclado.

--- app/src/main/java/felepo/contador/MainActivity.java
+++ app/src/main/java/felepo/contador/MainActivity.java
@@ -2,7 +2,10 @@ package felepo.contador;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,6 +24,31 @@ public class MainActivity extends Activity {
         textoResultado = (TextView) findViewById(R.id.contadorPulsaciones);
 
         contador = 0;
+
+        //Establecer el escuchador para el EditText de nReinicios
+        EventoTeclado teclado = new EventoTeclado();
+        EditText nReinicios = (EditText) findViewById(R.id.nReinicios);
+        nReinicios.setOnEditorActionListener(teclado);
+
+    }
+
+    /*
+        Clase para indicar el Evento cuando se presiona la tecla Done del teclado, el contador
+        se actualiza.
+    */
+    class EventoTeclado implements TextView.OnEditorActionListener
+    {
+
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent)
+        {
+            if( i == EditorInfo.IME_ACTION_DONE )
+            {
+                reiniciarContador(null);
+            }
+
+            return false;
+        }
     }
 
     public void incrementarContador(View vista)
@@ -59,7 +87,7 @@ public class MainActivity extends Activity {
 
     public void reiniciarContador(View vista)
     {
-        EditText numeroReset = (EditText) findViewById(R.id.inNumber);
+        EditText numeroReset = (EditText) findViewById(R.id.nReinicios);
         CheckBox casillaNegativo = (CheckBox) findViewById(R.id.negativos);
 
         try
@@ -78,5 +106,9 @@ public class MainActivity extends Activity {
 
         numeroReset.setText("");
         textoResultado.setText("" + contador);
+
+        //Código para ocultar el teclado luego de ingresar número de reinicio
+        InputMethodManager miTeclado = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        miTeclado.hideSoftInputFromWindow(numeroReset.getWindowToken(), 0);
     }
 }
